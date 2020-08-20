7d320b31774696b90407438659c8cada4991d4c6
Onkar7798 <onkarkajrolkar7798@gmail.com>
2020-08-21 01:12:17 +0530

    Search Invalidity addressed

--- flasktut.py
+++ flasktut.py
@@ -3,6 +3,7 @@ import csv
 
 
 app = Flask(__name__)
+app.secret_key = "qwerty"
 
 @app.route("/")
 def index():
@@ -13,7 +14,6 @@ def index():
 def add():
 	if request.method == "POST":
 		result = list(request.form.values());
-		#return render_template("add-student.html", result=result)
 		split = result[3].split('-')
 		result[3] = split[2]+"-"+split[1]+"-"+split[0]
 		with open('students.csv', 'a', newline='\n') as csv_file:
@@ -21,8 +21,21 @@ def add():
 			csv_writer.writerow(result)
 	return render_template("add-student.html")
 
-@app.route("/search")
+@app.route("/search", methods=["POST", "GET"])
 def search():
+	if request.method == "POST":
+		id = list(request.form.values())
+		print(id[0])
+		success = False
+		with open('students.csv', 'r') as csv_file:
+			csv_reader = csv.reader(csv_file)
+			for line in csv_reader:
+				if line[0] == id[0]:
+					details = line
+					success = True
+		if not success:
+			flash(f"Student id: {id} not found, Please try again!", "info")
+			
 	return render_template("search-student.html")
 
 @app.route("/display")
